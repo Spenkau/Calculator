a421322951dd8d843640c08a04abe01c8ad46a18
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-03-23 21:43:43 +0300

    added base styles & layout fixed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import "./styles.scss"
 
 function App() {
   return (
-    <div className="App">
-      <div className="device">
-
+      <div className="wrapper">
+          <div className="device">
+              <ul className="device-display">
+                  <li className="visor">
+                      <img width="21px" height="21px" src="/images/lens.png" alt="lens"/>
+                  </li>
+                  <li className="apps-list">
+                      <div className="calculator">
+                        <button className="to-calculator"></button>
+                        <p>Calculator</p>
+                      </div>
+                  </li>
+              </ul>
+          </div>
       </div>
-    </div>
   );
 }
 
