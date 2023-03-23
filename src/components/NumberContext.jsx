ae2ac232952db500660cbed2da499a080884c68e
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-03-23 16:58:04 +0300

    first settings and file hierarchy added

---
+++ src/components/NumberContext.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const NumberContext = React.createContext();
+
+const NumberProvider = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default NumberProvider;
